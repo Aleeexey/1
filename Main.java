89896707537d24c0ef49a2281d17e31038a1a38a
Aleeexey <52676619+Aleeexey@users.noreply.github.com>
2019-07-21 19:03:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 
-public class Main
-{public static void main (String[]args){
+public class Main{
+    public static void main (String[]args){
     /* коммент */
     System.out.println("HelloWorld!");
 }
