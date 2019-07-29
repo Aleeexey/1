bc68ddfcd53f39951e0af6f892cafcf0a7b1289b
Alex <Alex@mail.ru>
2019-07-29 22:35:28 +0300

    Java для начинающих №6: Отработка арифметических операций

---
+++ Xyz.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Xyz {
+    public static void main(String[] args) {
+        System.out.println("Введите поочередно три числа");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int z = scan.nextInt();
+        System.out.println((x + y + z) / 3);
+        System.out.println(((x + y + z) / 3) / 2);
+        if ((((x + y + z) / 3) / 2)>3) {
+            System.out.println("Программа выполнена корректно");
+
+        }
+    }
+}
