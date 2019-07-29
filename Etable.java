6cae957fa0dc7f1ac0f8dd4f9305cd023da7ccec
Alex <Alex@mail.ru>
2019-07-29 22:34:20 +0300

    Java для начинающих №5. Таблица умножения

---
+++ Etable.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Etable {
+    public static void main(String[] args) {
+        System.out.println("Введите число  и увидете таблицу умножения для него:");
+        Scanner scan=new Scanner(System.in);
+        int number = scan.nextInt();
+System.out.println(number);
+        System.out.println(number*2);
+        System.out.println(number*3);
+        System.out.println(number*4);
+        System.out.println(number*5);
+        System.out.println(number*6);
+        System.out.println(number*7);
+        System.out.println(number*8);
+        System.out.println(number*9);
+        System.out.println(number*10);
+
+    }
+}
