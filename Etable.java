b9ec8c5a280d5070f32fbca0c3dfc99a9944a970
Aleeexey <52676619+Aleeexey@users.noreply.github.com>
2019-08-11 20:22:38 +0300

    Update Etable.java

--- Etable.java
+++ Etable.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 public class Etable {
     public static void main(String[] args) {
-        System.out.println("Введите число  и увидете таблицу умножения для него:");
+        System.out.println("Введите число и система выдаст таблицу умножения");
         Scanner scan=new Scanner(System.in);
         int number = scan.nextInt();
-System.out.println(number);
-        System.out.println(number*2);
-        System.out.println(number*3);
-        System.out.println(number*4);
-        System.out.println(number*5);
-        System.out.println(number*6);
-        System.out.println(number*7);
-        System.out.println(number*8);
-        System.out.println(number*9);
-        System.out.println(number*10);
+        String a ="x1=";
+        String b ="x2=";
+        String c ="x3=";
+        String d="x4=";
+        String e ="x5=";
+        String f ="x6=";
+        String g ="x7=";
+        String h ="x8=";
+        String i ="x9=";
+        String j ="x10=";
+        System.out.println(number+a+number);
+        System.out.println(number+b+number*2);
+        System.out.println(number+c+number*3);
+        System.out.println(number+d+number*4);
+        System.out.println(number+e+number*5);
+        System.out.println(number+f+number*6);
+        System.out.println(number+g+number*7);
+        System.out.println(number+h+number*8);
+        System.out.println(number+i+number*9);
+        System.out.println(number+j+number*10);
 
     }
 }
